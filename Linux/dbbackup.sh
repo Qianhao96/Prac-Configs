ddf42c7364c410703998153bfcfba391e865b646
Qianhao Liu <qianhao.liu@gmail.com>
2022-07-05 20:32:47 +0800

    Cron job file and sql server backup shell script

---
+++ Linux/dbbackup.sh
@@ -0,0 +1,3 @@
+today=$(date +"%s")
+docker exec -it lqhdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Facaila666!' -Q "BACKUP DATABASE [Dev] TO DISK = N'/var/opt/mssql/data/dev.bak' WITH NOFORMAT, NOINIT, NAME = 'dev-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+docker cp lqhdb:/var/opt/mssql/data/dev.bak /home/lqh/dbbackups/dev_${today}.bak
